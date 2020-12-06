3a33d412f5833bd4f96f6b57896a2d688bd6afe7
Alex Trimm <alexander.trimm@gmail.com>
2020-12-06 13:30:40 -0500

    Update automata code, remove checkpoints

--- down/lib/automata/automata.py
+++ down/lib/automata/automata.py
@@ -39,11 +39,17 @@ def apply_filters(state, fs):
 def generate_1d(rule_number, init_state, n_rows):
     """ 
     Inputs:
-        rule_number - the Wolfram rule number that dictates which patterns calls for propagation
-        init_state - 
-        n_rows - 
+        rule_number - The Wolfram rule number that dictates which patterns calls for propagation
+        init_state - A numpy array to be used as the initial state of the automata
+        n_rows - The number of generations of the reproduction to produce
     Output:
         binary numpy ndarray of size (n_rows, len(init_state))
-
-
-
+    """
+    dims = init_state.shape
+    assert(len(dims)==1)
+    n_cols = dims[0]
+    fs = filters(rule_number)
+    rows = [init_state]
+    for _ in range(n_rows):
+        rows.append(apply_filters(rows[-1], fs))
+    return np.array(rows, dtype=np.bool)
